25e20ef7da0341eb8575318e50714b848f4f5864
jobsyu <1175585627@qq.com>
2015-08-07 11:38:55 +0800

    buy

---
+++ GroupBuy/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  GroupBuy
+//
+//  Created by jobs on 15/7/13.
+//  Copyright (c) 2015年 ycp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ GroupBuy/MapViewController.m
@@ -0,0 +1,13 @@
+//
+//  MapViewController.m
+//  GroupBuy
+//
+//  Created by jobs on 15/7/13.
+//  Copyright (c) 2015年 ycp. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@implementation MapViewController
+
+@end
